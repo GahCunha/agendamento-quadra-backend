f1517e20bf54dfe7e23ae5cce415ae1b86fd946d
Gabriel Cunha <75914436+GahCunha@users.noreply.github.com>
2025-03-19 13:36:58 -0300

    feat: adiciona documentação da API utilizando Swagger e integra ao servidor Express

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const { PORT } = require('./config');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Documentação disponível em http://localhost:${PORT}/api/docs`);
 });
