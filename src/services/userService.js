4f374dc19df551ed9033119683cbe9cd43e9e109
Gabriel Cunha <75914436+GahCunha@users.noreply.github.com>
2025-03-30 12:22:26 -0300

    feat: adiciona validação de e-mail para criação de usuário, permitindo apenas domínios @ifnmg.edu.br e @aluno.ifnmg.edu.br

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,12 +2,21 @@ const bcrypt = require("bcryptjs");
 const prisma = require("../prismaClient");
 
 const createUser = async ({ name, email, password }) => {
+  const allowedEmailRegex = /^[a-zA-Z0-9._%+-]+@(aluno\.)?ifnmg\.edu\.br$/;
+
+  email = email.toLowerCase();
+
+  if (!allowedEmailRegex.test(email)) {
+    throw new Error("E-mail não autorizado. Use um e-mail @ifnmg.edu.br ou @aluno.ifnmg.edu.br");
+  }
+
   const hashedPassword = await bcrypt.hash(password, 10);
 
   return prisma.user.create({
     data: { name, email, password: hashedPassword },
   });
 };
+
 
 const getUserById = async (id) => {
   return prisma.user.findUnique({ where: { id: parseInt(id) } });
